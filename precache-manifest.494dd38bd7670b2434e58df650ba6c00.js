0975a19e6700894105038cc4104518d039ce2a07
suneejan <8988770+suneejan@user.noreply.gitee.com>
2022-03-18 19:29:17 +0800

    deploy

---
+++ precache-manifest.494dd38bd7670b2434e58df650ba6c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653e62546b416d960a26",
+    "url": "css/app.caef73ab.css"
+  },
+  {
+    "revision": "da982d47c3dddea20294",
+    "url": "css/chunk-vendors.e8753d28.css"
+  },
+  {
+    "revision": "65c8b5fd395a694d762f6a812193cd9e",
+    "url": "index.html"
+  },
+  {
+    "revision": "653e62546b416d960a26",
+    "url": "js/app.ca368a81.js"
+  },
+  {
+    "revision": "da982d47c3dddea20294",
+    "url": "js/chunk-vendors.1542444f.js"
+  },
+  {
+    "revision": "87caf51c64178e1840b64b57110189f8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
